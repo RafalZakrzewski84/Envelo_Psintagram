298bcc0deaeb2015dffaa8a02891546a87f3b95c
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-20 22:05:09 +0200

    adding add dog select component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 import { MatCardModule } from '@angular/material/card'
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heros/heroes.component';
 import { DogCard } from './dogCard/dogCard.component'
+import { DogSelect } from './dogSelect/dogSelect.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
@@ -14,13 +16,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent,
     HeroesComponent,
     DogCard,
+    DogSelect
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
