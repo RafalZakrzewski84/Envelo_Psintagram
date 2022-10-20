e18693a5e8d5a20e983734d774fd8736c34ca254
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-20 20:33:36 +0200

    adding dog card

---
+++ src/app/dogCard/dogCard.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+/**
+ * @title Card with multiple sections
+ */
+@Component({
+    selector: 'dog-card',
+    templateUrl: './dogCard.component.html',
+    styleUrls: ['./dogCard.component.css'],
+})
+export class DogCard implements OnInit {
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+}
